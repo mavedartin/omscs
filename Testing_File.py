3afacf6d0630e08b9e3fe50b4606add07c9362d5
mavedartin <davidamartin08@gmail.com>
2020-06-29 14:57:07 -0700

    Branch 1 Test File modification to Master

--- Testing_File.py
+++ Testing_File.py
@@ -12,3 +12,10 @@ print("This was added from Testing_Branch one")
 # I am going to commit this to the Master Local branch
 # I did not see anything to offered a review or commit and merge. But this must have worked because its now in the master branch
     # now commit to the remote master
+
+
+# Modification made from Branch 1
+x = 10
+y = 20
+z = x + y
+print(z)
