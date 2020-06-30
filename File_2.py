67951be38b44f032eab087f4ac32fddda371a60d
mavedartin <davidamartin08@gmail.com>
2020-06-29 19:34:53 -0700

    Delete File_2.py

--- File_2.py
+++
@@ -1,8 +0,0 @@
-# File 2 from Branch 2
-print('initialized in Branch 2')
-print('Try to merge this into Branch 1')
-# Lets get this loaded in Github
-
-
-#Modifying this file to test how the versioning is working
-print("testing this shiiiiit")
